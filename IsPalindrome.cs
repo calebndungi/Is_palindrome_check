b8b1f71beaecce732902096766fd4c1d355680d5
caleb ndungi <calebndungi@gmail.com>
2021-07-30 12:10:21 +0300

    Is Palindrome Check
    Check if string is palindrome

---
+++ IsPalindrome.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Is_Palindrome_Check
+{
+    public class IsPalindrome
+    {
+        public static bool recursiveCheck(string str)
+        {
+
+
+            return true;
+        }
+
+        public static bool pointersCheck(string str)
+        {
+
+            return true;
+        }
+
+        public static bool reverseArray(string str)
+        {
+            
+            return true;
+        }
+        public static bool StartsWithUpper(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            char ch = str[0];
+            return char.IsUpper(ch);
+        }
+
+    }
+}
