36438a45088e89b7703afc7e796ba5ad9d24b596
calebndungi <calebndungi@gmail.com>
2021-08-03 08:34:37 +0300

    Adding Recursion and Reverse Check
    Adding Recursion and Reverse Check

--- IsPalindrome.cs
+++ IsPalindrome.cs
@@ -10,9 +10,23 @@ namespace Is_Palindrome_Check
     {
         public static bool recursiveCheck(string str)
         {
-
-
-            return true;
+            //best case is when str has only one character
+            if (str.Length <= 1)
+            {
+                return true;
+            }
+            else
+            {   
+                //if the first character is not equal to the last character 
+                if (str[0] != str[str.Length - 1])
+                {
+                    return false;
+                }
+                else
+                {
+                    return recursiveCheck(str.Substring(1, str.Length - 2));
+                }
+            }
         }
 
         public static bool pointersCheck(string str)
@@ -24,7 +38,24 @@ namespace Is_Palindrome_Check
         public static bool reverseArray(string str)
         {
             
-            return true;
+            string reverse = string.Empty;
+
+            //decrementing loop from the last char to the first
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reverse += str[i];
+            }
+
+            //compare if reversed is the same as the original string
+            if (str == reverse)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
         }
         public static bool StartsWithUpper(string str)
         {
--- StringLibraryTest/UnitTest1.cs
+++ StringLibraryTest/UnitTest1.cs
@@ -50,5 +50,53 @@ namespace StringLibraryTest
                                      word == null ? "<null>" : word, result));
             }
         }
+
+        [TestMethod]
+        public void CheckRecurssive()
+        {
+            // tests if a string is a palindrome.
+            string words = "aabaa";
+
+            bool result = IsPalindrome.recursiveCheck(words);
+
+            Assert.IsTrue(result, String.Format("Expected for '{0}' : true Actual: {1}", words, result));
+            
+        }
+
+        [TestMethod]
+        public void CheckNotRecurssive()
+        {
+            // tests if a string is a palindrome.
+            string words = "aabana";
+
+            bool result = IsPalindrome.recursiveCheck(words);
+
+            Assert.IsFalse(result, String.Format("Expected for '{0}' : true Actual: {1}", words, result));
+
+        }
+
+        [TestMethod]
+        public void CheckReverseTrue()
+        {
+            // tests if a string is a palindrome.
+            string words = "abcba";
+
+            bool result = IsPalindrome.reverseArray(words);
+
+            Assert.IsTrue(result, String.Format("Expected for '{0}' : true Actual: {1}", words, result));
+
+        }
+
+        [TestMethod]
+        public void CheckReverseFalse()
+        {
+            // tests if a string is a palindrome.
+            string words = "abjcba";
+
+            bool result = IsPalindrome.reverseArray(words);
+
+            Assert.IsFalse(result, String.Format("Expected for '{0}' : true Actual: {1}", words, result));
+
+        }
     }
 }
